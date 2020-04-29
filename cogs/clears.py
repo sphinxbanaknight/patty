80f8d8328c3e41c5a4de60c1840c849ce2e6dabc
sphinxbanaknight <63153344+sphinxbanaknight@users.noreply.github.com>
2020-04-29 09:36:51 +0800

    Update clears.py

--- cogs/clears.py
+++ cogs/clears.py
@@ -1312,15 +1312,17 @@ Thank you!```\n""")
 
             for x in range(len(ign)):
                 ignlist += ign[x] + '\n'
-                no_of_pref = len([x.strip() for x in pref[x].split(';')])
-                if no_of_pref > 8:
-                    ignlist += '\n'
+                if x != 0:
+                    no_of_pref = len([x.strip() for x in pref[x-1].split(';')])
+                    if no_of_pref > 8:
+                        ignlist += '\n'
             x = 0
             for x in range(len(role)):
                 classlist += role[x] + '\n'
-                no_of_pref = len([x.strip() for x in pref[x].split(';')])
-                if no_of_pref > 8:
-                    classlist += '\n'
+                if x != 0:
+                    no_of_pref = len([x.strip() for x in pref[x-1].split(';')])
+                    if no_of_pref > 8:
+                        classlist += '\n'
             x = 0
             for x in range(len(pref)):
                 preflist += pref[x] + '\n'

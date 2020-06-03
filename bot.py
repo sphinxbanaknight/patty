e878c95c40b43e898f72c01ea1de02d1ce062b58
sphinxbanaknight <63153344+sphinxbanaknight@users.noreply.github.com>
2020-06-04 07:32:12 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -259,7 +259,7 @@ async def huhubes(ctx):
     attlist = [item for item in rostersheet.col_values(7) if item and item != 'IGN' and item != 'Next WOE:']
     ignlist = [item for item in rostersheet.col_values(3) if item and item != 'IGN' and item != 'READ THE NOTES AT [README]']
     
-    #print(f'{attlist} at {ignlist}')
+    print(f'{attlist} at {ignlist}')
     
     for ign in ignlist:
         for att in attlist:

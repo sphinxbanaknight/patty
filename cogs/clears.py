bea6e4d88675836a524ad9e8f4ab165287cbb8da
sphinxbanaknight <63153344+sphinxbanaknight@users.noreply.github.com>
2020-06-19 22:21:05 +0800

    Update clears.py

--- cogs/clears.py
+++ cogs/clears.py
@@ -1489,7 +1489,7 @@ PLEASE MIND THE COMMA, IT ENSURES THAT I SEE EVERY ARGUMENT:
             for x in range(len(role)):
                 classlist += role[x] + '\n'
                 no_of_pref = len([x.strip() for x in pref[x].split(';')])
-                if no_of_pref > 7:
+                if no_of_pref > 7 or (no_of_pref > 6 and len(ign[x]) > 11):
                     classlist += '\n'
                     if no_of_pref > 12:
                         ignlist += '\n'

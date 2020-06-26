14ef70569b7f5d1b79167de254c58d57e9d5e170
sphinxbanaknight <63153344+sphinxbanaknight@users.noreply.github.com>
2020-06-26 10:36:59 +0800

    Update clears.py

--- cogs/clears.py
+++ cogs/clears.py
@@ -1486,7 +1486,7 @@ PLEASE MIND THE COMMA, IT ENSURES THAT I SEE EVERY ARGUMENT:
                 #print(f'{x}')
                 #print(f'{ign[x]}')
                 #print(f'{no_of_pref}')
-                if (no_of_pref > 6 and len(ign[x]) > 11):
+                if no_of_pref > 7 or (no_of_pref > 7 and len(ign[x]) > 11):
                     ignlist += '\n'
                     #print('pumasok1')
                     #if no_of_pref > 12:
@@ -1496,7 +1496,7 @@ PLEASE MIND THE COMMA, IT ENSURES THAT I SEE EVERY ARGUMENT:
             for x in range(len(role)):
                 classlist += role[x] + '\n'
                 no_of_pref = len([x.strip() for x in pref[x].split(';')])
-                if no_of_pref > 7 or (no_of_pref > 6 and len(ign[x]) > 11):
+                if no_of_pref > 7 or (no_of_pref > 7 and len(ign[x]) > 11):
                     classlist += '\n'
                     #if no_of_pref > 12:
                     #    classlist += '\n'
